eb4bd234178083786e04bd27e5c0278c5f3ba43f
Curtis Shea <curtis.shea@gmail.com>
2023-04-18 22:33:25 +0100

    Added view predictions page and table

--- app.js
+++ app.js
@@ -1,51 +1,64 @@
 // Replace FIREBASE_CONFIG with your actual Firebase configuration object
 const firebaseConfig = {
-    // ...
+  apiKey: "AIzaSyBe-7yurBiv1JQRPkeINtwCxDXUQXs6m5Y",
+  authDomain: "baby-shea-predictions.firebaseapp.com",
+  projectId: "baby-shea-predictions",
+  storageBucket: "baby-shea-predictions.appspot.com",
+  messagingSenderId: "263569611047",
+  appId: "1:263569611047:web:e727099ffe322d17d57102",
+  measurementId: "G-EY05RNJCEB"
 };
 
 firebase.initializeApp(firebaseConfig);
 const db = firebase.firestore();
 
 const form = document.getElementById("prediction-form");
-const predictionsList = document.getElementById("predictions-list");
+const predictionsTableBody = document.querySelector("#predictions-table tbody");
 
-form.addEventListener("submit", async (e) => {
+if (form) {
+  form.addEventListener("submit", async (e) => {
     e.preventDefault();
 
     const name = form.name.value;
     const dob = form.dob.value;
     const weight = form.weight.value;
     const sex = form.sex.value;
-    const eyeColor = form["eye-color"].value;
-    const hairColor = form["hair-color"].value;
-    const fullName = form["full-name"].value;
+    const eyeColor = form.eyeColor.value;
+    const hairColor = form.hairColor.value;
+    const fullName = form.fullName.value;
 
     await db.collection("predictions").add({
-        name,
-        dob,
-        weight,
-        sex,
-        eyeColor,
-        hairColor,
-        fullName,
+      name,
+      dob,
+      weight,
+      sex,
+      eyeColor,
+      hairColor,
+      fullName
     });
 
     form.reset();
-});
-
-// Function to create a list item for a prediction
-function createPredictionItem(prediction) {
-    const li = document.createElement("li");
-    li.textContent = `${prediction.name} predicts ${prediction.fullName} will be born on ${prediction.dob} weighing ${prediction.weight} lbs, with ${prediction.eyeColor} eyes, ${prediction.hairColor} hair, and will be a ${prediction.sex}.`;
-    return li;
+  });
 }
 
-// Fetch and display the predictions
-db.collection("predictions").orderBy("name").onSnapshot((snapshot) => {
-    predictionsList.innerHTML = "";
+if (predictionsTableBody) {
+  // Load predictions from Firestore and display them in the table
+  db.collection("predictions").onSnapshot((snapshot) => {
+    predictionsTableBody.innerHTML = "";
     snapshot.forEach((doc) => {
-        const prediction = doc.data();
-        const li = createPredictionItem(prediction);
-        predictionsList.appendChild(li);
+      const data = doc.data();
+      const row = `
+        <tr>
+          <td>${data.name}</td>
+          <td>${data.sex}</td>
+          <td>${data.dob}</td>
+          <td>${data.weight}</td>
+          <td>${data.eyeColor}</td>
+          <td>${data.hairColor}</td>
+          <td>${data.fullName}</td>
+        </tr>
+      `;
+      predictionsTableBody.innerHTML += row;
     });
-});
+  });
+}
